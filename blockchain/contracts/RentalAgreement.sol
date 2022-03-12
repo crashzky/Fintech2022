316ddf3501d93818a49f0d402d4886963947396c
Fedor Ivanov <ivnfedor@gmail.com>
2022-03-12 17:47:35 +0300

    Initial commit from template [skip ci]

---
+++ blockchain/contracts/RentalAgreement.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+contract RentalAgreement {
+    function sayHelloWorld() external pure returns (string memory) {
+        return "Hello, world!";
+    }
+}
