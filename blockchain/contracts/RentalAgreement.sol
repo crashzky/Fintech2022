761c77d2531c3c3a01e31c903e0afe38cc5a35af
iinkaaa <ashurova.2005@mail.ru>
2022-03-15 10:09:06 +0300

    us1

--- blockchain/contracts/RentalAgreement.sol
+++ blockchain/contracts/RentalAgreement.sol
@@ -5,4 +5,19 @@ contract RentalAgreement {
     function sayHelloWorld() external pure returns (string memory) {
         return "Hello, world!";
     }
+
+    uint roomID;
+    address ladd;
+
+    mapping(address => uint) data;
+
+    constructor (address _ladd, uint _roomID) {
+        data[_ladd] = _roomID;
+        ladd = _ladd;
+    }
+
+    function getRoomInternalId() public view returns(uint) {
+        return data[ladd];
+    }
+
 }
