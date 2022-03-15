caa037ca984611341fc4b3cd0ecb00a7d810bb7e
Yan Kurbatov <deknowny@gmail.com>
2022-03-15 16:51:17 +0300

    f9

--- backend/src/storage.py
+++ backend/src/storage.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 addresses_messages = {}
-conn = sqlite3.connect("./nto-moll-db.sqlite3")
+conn = sqlite3.connect("/nto-moll-db.sqlite3")
 conn.row_factory = sqlite3.Row
 db = conn.cursor()
 # smells bad, I know
