3838b7d50313335ca5b5408d3ce2e9d5d57d9fd6
Yan Kurbatov <deknowny@gmail.com>
2022-03-16 12:57:09 +0300

    try again

--- backend/src/env.py
+++ backend/src/env.py
@@ -1,6 +1,6 @@
 import os
 
-LANDLORD_ADDRESS = os.getenv("LANDLORD_ADDRESS")
+LANDLORD_ADDRESS = os.getenv("LANDLORD_ADDRESS") or "0x123"
 RPC_URL = os.getenv("RPC_URL")
 
 if LANDLORD_ADDRESS is None:
