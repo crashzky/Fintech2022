e4f44e7b3583f10e509c51c5800fb13238374d5e
Kirill Osintsev <osintsev.2000@inbox.ru>
2022-03-16 20:38:26 +0500

    fix auth

--- frontend/src/shared/api/contract.ts
+++ frontend/src/shared/api/contract.ts
@@ -1,3 +1,34 @@
-const getRentStartTime = (contractAddress: string) => {
-	
+import CONTRACT_CODE from '../consts/contract';
+import Web3 from 'web3';
+
+const solc = require('solc');
+
+const getRentStartTime = (contractAddress: string): Promise<any> => {
+	const web3 = new Web3((window as any).ethereum);
+
+	const input = {
+		language: 'Solidity',
+		sources: {
+		  'test.sol': {
+			content: CONTRACT_CODE,
+		  }
+		},
+		settings: {
+		  outputSelection: {
+			'*': {
+			  '*': ['*']
+			}
+		  }
+		}
+	};
+
+	const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+	const contract = new web3.eth.Contract(output as any, contractAddress);	
+
+	return contract.methods.getRentStartTime().call();
+};
+
+export {
+	getRentStartTime,
 };
