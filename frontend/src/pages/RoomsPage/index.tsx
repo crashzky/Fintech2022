c105665d5eb8fd4f0292f58a3ade3c5fc09fff86
Kirill Osintsev <osintsev.2000@inbox.ru>
2022-03-16 19:32:17 +0500

    fix auth

--- frontend/src/pages/RoomsPage/index.tsx
+++ frontend/src/pages/RoomsPage/index.tsx
@@ -2,15 +2,20 @@ import { Link } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import { checkAuntefication } from '../../shared/api/auth';
 import { useEffect, useState } from 'react';
+import Web3 from 'web3';
 
 const RoomsPage = (): JSX.Element => {
 	const [isLandlord, setIsLandlord] = useState(false);
 
 	const { data } = useQuery('auth', checkAuntefication);
 
+	const web3 = new Web3((window as any).ethereum);
+
 	useEffect(() => {
-		if(localStorage.getItem('is_landlord') === data?.data.authentication.address) 
-			setIsLandlord(true);
+		web3.eth.requestAccounts().then((res: any) => {
+			if(res[0]=== data?.data.authentication.address) 
+				setIsLandlord(true);
+		});
 	}, [data]);
 
 	if(isLandlord) {
