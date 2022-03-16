0463d5eb2e98f3d2b26bc88c02ea609df4131c10
Kirill Osintsev <osintsev.2000@inbox.ru>
2022-03-16 19:09:21 +0500

    fixed check isLandlord

--- frontend/src/pages/RoomsPage/index.tsx
+++ frontend/src/pages/RoomsPage/index.tsx
@@ -1,7 +1,19 @@
 import { Link } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { checkAuntefication } from '../../shared/api/auth';
+import { useEffect, useState } from 'react';
 
 const RoomsPage = (): JSX.Element => {
-	if(localStorage.getItem('is_landlord') === 'true') {
+	const [isLandlord, setIsLandlord] = useState(false);
+
+	const { data } = useQuery('auth', checkAuntefication);
+
+	useEffect(() => {
+		if(localStorage.getItem('is_landlord') === data?.data.authentication.address) 
+			setIsLandlord(true);
+	}, [data]);
+
+	if(isLandlord) {
 		return (
 			<Link to='/rooms/create' className='rooms__create' />
 		);
