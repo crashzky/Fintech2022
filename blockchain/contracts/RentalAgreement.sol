d955cd83e164029f615a8c45219136e9a7aed50d
Yan Kurbatov <deknowny@gmail.com>
2022-03-17 13:45:54 +0300

    ADd iterable map

--- blockchain/contracts/RentalAgreement.sol
+++ blockchain/contracts/RentalAgreement.sol
@@ -8,8 +8,65 @@ struct Sign {
     bytes32 s;
 }
 
+library IterableMapping {
+    // Iterable mapping from address to uint;
+    struct Map {
+        address[] keys;
+        mapping(address => uint) values;
+        mapping(address => uint) indexOf;
+        mapping(address => bool) inserted;
+    }
+
+    function get(Map storage map, address key) public view returns (uint) {
+        return map.values[key];
+    }
+
+    function getKeyAtIndex(Map storage map, uint index) public view returns (address) {
+        return map.keys[index];
+    }
+
+    function size(Map storage map) public view returns (uint) {
+        return map.keys.length;
+    }
+
+    function set(
+        Map storage map,
+        address key,
+        uint val
+    ) public {
+        if (map.inserted[key]) {
+            map.values[key] = val;
+        } else {
+            map.inserted[key] = true;
+            map.values[key] = val;
+            map.indexOf[key] = map.keys.length;
+            map.keys.push(key);
+        }
+    }
+
+    function remove(Map storage map, address key) public {
+        if (!map.inserted[key]) {
+            return;
+        }
+
+        delete map.inserted[key];
+        delete map.values[key];
+
+        uint index = map.indexOf[key];
+        uint lastIndex = map.keys.length - 1;
+        address lastKey = map.keys[lastIndex];
+
+        map.indexOf[lastKey] = index;
+        delete map.indexOf[key];
+
+        map.keys[index] = lastKey;
+        map.keys.pop();
+    }
+}
+
 
 contract RentalAgreement {
+    using IterableMapping for IterableMapping.Map;
     event PurchasePayment(uint amount);
     // From constructor
     uint globalRoomInternalID;
@@ -24,7 +81,7 @@ contract RentalAgreement {
     bool globalIsRented = false;
 
     // Cashiers
-    mapping(address => uint) cashiers;
+    IterableMapping.Map cashiers;
     address[] public cashiersList;
     uint cashierIncrement = 1;
     uint cashierDecrement = 0;
@@ -150,8 +207,7 @@ contract RentalAgreement {
             revert("Zero address cannot become a cashier");
         }
         // Commit it
-        cashiers[addr] = ++cashierIncrement;
-        cashiersList.push(addr);
+        cashiers.set(addr, ++cashierIncrement);
     }
 
     // Check if cashier exists
@@ -166,22 +222,17 @@ contract RentalAgreement {
             revert("Unknown cashier");
         }
 
-        cashierDecrement++;
-        delete cashiers[cashierAddr];
+        cashiers.remove(cashierAddr);
 
-        address[] memory newCashiersList;
-        uint256 newCashiersIndex = 0;
-        for (uint i = 0; i < cashiersList.length - 1; i++) {
-            if (cashiersList[i] != cashierAddr) {
-                newCashiersList[newCashiersIndex] = cashiersList[i];
-                newCashiersIndex++;
-            }
-        }
-        cashiersList = newCashiersList;
     }
 
     function getCashiersList() view public returns (address[] memory) {
-        return cashiersList;
+        address[] memory responseCashiers;
+        for (uint i = 0; i < cashiers.size(); i++) {
+            address key = map.getKeyAtIndex(i);
+            responseCashiers[i] = key;
+        }
+        return responseCashiers;
     }
 
     function pay(uint deadline, uint nonce, uint value, Sign memory cashierSign) payable public {
