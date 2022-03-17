11ba3bcebddae1fe9079c6a4e5e9741d529f1172
Kirill Osintsev <osintsev.2000@inbox.ru>
2022-03-17 18:38:03 +0500

    finalazi

--- frontend/src/pages/RoomPage/[id].tsx
+++ frontend/src/pages/RoomPage/[id].tsx
@@ -139,9 +139,6 @@ const RoomPage = (): JSX.Element => {
 			<p className='room__internal-name '>
 				{data && data.data.room.internalName}
 			</p>
-			<p>
-				{rentEndTime ? rentEndTimeNum : 'none time'}
-			</p>
 			{/*(data && data?.data && (getStatus() === 'Rented' || getStatus() === 'Rent ended')) && (
 				<>
 					<p className='room__tenant'>
